3fe133dc9410a9213935b558f2c5cb5d942f9084
Upendra Pratap Kushwaha <upendrapratap001@gmail.com>
2015-08-04 17:09:57 +0530

    Front end Html generate

--- functional_tests.py
+++ functional_tests.py
@@ -35,7 +35,8 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
-
+        import time
+        time.sleep(10)
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
