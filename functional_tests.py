e515406123711fbbf705459f4e186ea310b27377
Upendra Pratap Kushwaha <upendrapratap001@gmail.com>
2015-08-04 11:21:53 +0530

    Use unittest in testing

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,22 @@
 from selenium import webdriver
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+import unittest
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
 
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicity_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrive_it_later(self):
+        #to check out it's homepage
+        self.browser.get('http://localhost:8000')
+
+        #notice the page title
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test !')
+
+if __name__== '__main__':
+    unittest.main()
