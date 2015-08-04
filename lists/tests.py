937768c9d5c176ef2cf24dbb222e20132a56ff22
Upendra Pratap Kushwaha <upendrapratap001@gmail.com>
2015-08-04 13:07:40 +0530

    Basic view with return minimul HTML

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from lists.views import home_page
+from django.http import HttpRequest
 
 # Create your tests here.
 
@@ -14,3 +15,11 @@ class HomePageTest(TestCase):
     def test_root_url_resolve_to_home_page(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith('</html>'))
+
