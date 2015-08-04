699b7d0333d048eb50d6c5fac84ce728717467dd
Upendra Pratap Kushwaha <upendrapratap001@gmail.com>
2015-08-04 15:16:20 +0530

    Functional test now check an item is added on page

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import unittest
+from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -16,6 +17,30 @@ class NewVisitorTest(unittest.TestCase):
 
         #notice the page title
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        #invite to enter a to-do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+               inputbox.get_attribute('placeholder'),
+               'Enter a to-do item'
+                )
+
+
+        # I typed "Buy peacock feathers" into the text box
+        inputbox.send_keys('Buy peacock feathers')
+
+        # When i hit enter the page updates and now the page list
+        # "1: Buy peacock feathers" as an item in to-do list table
+        inputbox.send_keys(keys.ENTER)
+
+        table = self.brower.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+                any(row.text == '1: Buy peacock feathers' for row in rows)
+               )
+        # There is still a textbox inviting me to add a to-do
         self.fail('Finish the test !')
 
 if __name__== '__main__':
