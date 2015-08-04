f2a4978d0d0efbf6823276fd39d713ae89d088ce
Upendra Pratap Kushwaha <upendrapratap001@gmail.com>
2015-08-04 12:40:19 +0530

    My frist unit test with dummy view

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,16 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from lists.views import home_page
 
 # Create your tests here.
 
 class SmokeTest(TestCase):
 
     def test_bad_maths(self):
-        self.assertEqual(1+1, 3)
+        self.assertEqual(1+1, 2)
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolve_to_home_page(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
