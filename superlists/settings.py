3ccde30790554d1868537d5ee5e9d64d7c28ea13
Upendra Pratap Kushwaha <upendrapratap001@gmail.com>
2016-01-04 17:57:28 +0530

    Revert database-url changes

--- superlists/settings.py
+++ superlists/settings.py
@@ -84,9 +84,9 @@ DATABASES = {
     }
 }
 # Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES = {}
-DATABASES['default'] =  dj_database_url.config()
+# import dj_database_url
+# DATABASES = {}
+# DATABASES['default'] =  dj_database_url.config()
 
 
 # Internationalization
