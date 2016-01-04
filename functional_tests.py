8d212865e9fad4063ad1b4a84f60381e479f8bee
Upendra Pratap Kushwaha <upendrapratap001@gmail.com>
2016-01-04 11:42:32 +0530

    added 2 item check in functional test

--- functional_tests.py
+++ functional_tests.py
@@ -39,16 +39,21 @@ class NewVisitorTest(unittest.TestCase):
         #time.sleep(10)
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table --it's text was:\n%s" % (
-                table.text)
-        )
+        # self.assertTrue(
+        #     any(row.text == '1: Buy peacock feathers' for row in rows),
+        #     "New to-do item did not appear in table --it's text was:\n%s" % (
+        #         table.text)
+        # )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
+        self.assertIn(
+            '2: Use peacock feathers to make a fly' ,
+             [row.text for row in rows]
+        )
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
         # methodical)
-        self.fail('Finish the test!')
+        #self.fail('Finish the test!')
 
         # The page updates again, and now shows both items on her list
 
